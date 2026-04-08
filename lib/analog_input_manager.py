a85abe036f07a995c9958e520bd4b0fb0aec8313
kyle <shannonkyle.deleon@gmail.com>
2026-04-07 19:31:37 -0700

    ANALOG: add debouncing, and event callbacks

--- lib/analog_input_manager.py
+++ lib/analog_input_manager.py
@@ -1,6 +1,8 @@
 """
     Analog input manager: init + asyncio task for MCP23017 inputs.
 """
+import time
+
 import uasyncio as asyncio
 import utils.mcp23017
 from machine import I2C, Pin
@@ -12,6 +14,7 @@ _SDA_PIN = 32
 _FREQ = 400000
 _ADDRESS = 0x20
 _BUTTON_PINS = [0, 1]
+_BUTTON_DEBOUNCE_MS = 50
 _POLL_INTERVAL_MS = 10
 
 class AnalogInputManager:
@@ -26,11 +29,54 @@ class AnalogInputManager:
 
         # Configure interrupt
         self.mcp.config(interrupt_polarity=0, interrupt_mirror=1)
+
+        # Set up call backs to have other apps be notified of inputs
+        self._callbacks = {pin: [] for pin in _BUTTON_PINS}
+        self._last_state =  {pin: False for pin in _BUTTON_PINS}
+        self._last_change = {pin: time.ticks_ms() for pin in _BUTTON_PINS}
+        self._debounce_ms = _BUTTON_DEBOUNCE_MS
     
+    def get_i2c(self):
+        """Return the I2C instance for external use."""
+        return self.i2c
+    
+    def register_callback(self, pin, callback, *args, **kwargs):
+        """Register a callback for a specific pin."""
+        if pin in self._callbacks:
+            self._callbacks[pin].append((callback, args, kwargs))
+    
+    def unregister_callback(self, pin, callback):
+        """Unregister a callback for a specific pin."""
+        if pin in self._callbacks and callback in self._callbacks[pin]:
+            self._callbacks[pin].remove(callback)
+
     async def poll_inputs(self):
         """Continuously poll the configured pins"""
         while True:
+            now = time.ticks_ms()
             for pin in _BUTTON_PINS:
-                if self.mcp.pin(pin):
-                    print(f"Button {pin} is pressed")
+                # Read current state of the pin and register an event if any button pressed
+                pressed = bool(self.mcp.pin(pin))
+                if (
+                    pressed != self._last_state[pin] and 
+                    time.ticks_diff(now, self._last_change[pin]) > self._debounce_ms
+                ):
+                    self._last_state[pin] = pressed
+                    self._last_change[pin] = now
+                    if pressed:
+                        event = {"pin": pin, "timestamp": now}
+                        print(f"Button press: {event}")
+                        
+                        # Handle callbacks
+                        for callback, args, kwargs in self._callbacks[pin]:
+                            try: 
+                                # Call the callback
+                                result = callback(event, *args, **kwargs)
+
+                                # Handle if callback is async
+                                if hasattr(result, "__await__"):
+                                    asyncio.create_task(result)
+                            except Exception as e:
+                                print("Callback error:", e)
+
             await asyncio.sleep_ms(_POLL_INTERVAL_MS)
--- main.py
+++ main.py
@@ -19,7 +19,15 @@ async def _scheduler_loop():
     analog_input_manager = AnalogInputManager()
     tasks.append(asyncio.create_task(analog_input_manager.poll_inputs()))
 
+    # Handle time management
+    time_manager = TimeManager(analog_input_manager.get_i2c())
+    analog_input_manager.register_callback(
+        0,
+        time_manager.set_time,
+        (2024, 6, 1, 5, 12, 0, 0, 0))
+
     while True:
+        print("Current time:", time_manager.get_time())
         await asyncio.sleep(1)
 
 def main():
